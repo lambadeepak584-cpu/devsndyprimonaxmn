f48f9648ed0d9b08ead103c470394de132d0fb20
lambadeepak584-cpu <lambadeepak584@gmail.com>
2026-08-13 05:55:33 +0000

    onhre

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1320634142" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2966635158" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
